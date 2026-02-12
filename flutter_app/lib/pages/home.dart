cb0157558dec2933bda2d44a9dfe1d2197edba28
kristine-coder <megrelishvilikristine@gmail.com>
2026-02-13 00:09:09 +0400

    i have added new icons for the breakfast app and also created a new model for the categories. I have also made some changes to the home page to display the categories and the new icons. The pubspec.lock file has been updated to include the new assets.

--- flutter_app/lib/pages/home.dart
+++ flutter_app/lib/pages/home.dart
@@ -1,109 +1,205 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/category_model.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
-    const HomePage({super.key});
+   HomePage({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: appBar(),
-            body: Column(
-                children: [
-                    Container(
-                        margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-                        decoration: BoxDecoration(
-                            boxShadow: [
-                                BoxShadow(
-                                    color: Color(0xff1D1617).withValues(alpha: 0.11),
-                                    blurRadius: 40,
-                                    spreadRadius: 0.0, 
-                                )
-                            ]
-                        ),
-                        child: TextField(
-                        decoration: InputDecoration(
-                           filled: true, 
-                           fillColor: Colors.white,
-                           contentPadding: EdgeInsets.all(15),
-                           prefixIcon: Padding(
-                             padding: const EdgeInsets.all(12.0),
-                             child: SvgPicture.asset('assets/icons/Search.svg'),
-                           ),
-                           suffixIcon: IntrinsicHeight(
-                             child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                               children: [
-                                  VerticalDivider(
-                                      color: Color(0xffDDDADA),
-                                      thickness: 0.1,
-                                  ),
-                                  Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: SvgPicture.asset('assets/icons/filter.svg'),
-                                  )
-                               ],
-                             ),
-                           ),
-                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15), 
-                            borderSide: BorderSide.none
-                           ),  
-                        ),
-                    ))
-               ],
-           ),
-        );
-    }
+  List<CategoryModel> categories = [];
 
-    AppBar appBar() {
-        return AppBar(
-            title: Text(
-                'Breakfast',
+  void _getCategories(){
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getCategories();
+    return Scaffold(
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _searchField(),
+          SizedBox(height: 40),
+          _categoriesSection()
+        ],
+      ),
+    );
+  }
+
+  Column _categoriesSection() {
+    return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Category',
                 style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                ),
+                  color: Colors.black, 
+                  fontSize: 18, 
+                  fontWeight: FontWeight.w600
+                )
+              ),
             ),
-            backgroundColor: Colors.white,
-            elevation: 0.0,
-            centerTitle: true,
-            leading: GestureDetector(
-                onTap: () {},
-                child: Container(
-                    margin: EdgeInsets.all(10),
-                    alignment: Alignment.center,
+            SizedBox(height: 15,),
+            Container(
+              height: 120,
+              child: ListView.separated(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                separatorBuilder: (context, index) => SizedBox(width: 25),
+                itemBuilder: (context, index){
+                  return Container(
+                    width:100, 
                     decoration: BoxDecoration(
-                        color: Color(0xfff7f8f8),
-                        borderRadius: BorderRadius.circular(10),
+                      color: categories[index].boxColor.withValues(alpha: 0.3),
+                      borderRadius: BorderRadius.circular(16)
                     ),
-                    child: SvgPicture.asset(
-                        'assets/icons/Arrow - Left 2.svg',
-                        height: 20,
-                        width: 20,
-                    ),
-                ),
-            ),
-            actions: [
-                GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                        margin: EdgeInsets.all(10),
-                        alignment: Alignment.center,
-                        width: 37,
-                        decoration: BoxDecoration(
-                            color: Color(0xfff7f8f8),
-                            borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: SvgPicture.asset(
-                            'assets/icons/dots.svg',
-                            height: 5,
-                            width: 5,
-                        ),
-                    ),
-                ),
-            ],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                      Column(
+                        children: [
+                          Container(
+                            width: 50, 
+                            height: 50, 
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SvgPicture.asset(categories[index].iconPath),
+                            ),
+                          ),
+                          Text(
+                            categories[index].name, 
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                              color: Colors.black, 
+                            ),
+                          ),
+                        ],
+                      )
+                    ],),
+                  ); 
+                } ),
+
+            )
+          ],
         );
-    }
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff1D1617).withValues(alpha: 0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0,
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search for Breakfast',
+              hintStyle: TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14, 
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SvgPicture.asset('assets/icons/Search.svg'),
+              ),
+              suffixIcon: Container(
+                width: 100,       
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+              ),
+            ),
+          ),
+        );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xfff7f8f8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+              color: Color(0xfff7f8f8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
